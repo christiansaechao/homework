4c3b32f0d987d339e2f3a05398394ccc61af1d87
christiansaechao <62818511+christiansaechao@users.noreply.github.com>
2025-02-22 19:39:23 -0800

    created styled card

---
+++ src/assignments/StyledCard/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.scss";
+
+const StyledCard = () => {
+
+    return (
+        <div className="styled-card-container">
+            <h1>John Doe</h1>
+            <h4>Software Engineer</h4>
+        </div>
+    )
+}
+
+export default StyledCard;
